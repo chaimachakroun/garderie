293c5476932a5a4e69273bed15f32b1ecc670538
Walidbelhadj <47835010+Walidbelhadj@users.noreply.github.com>
2020-05-28 16:41:44 +0100

    Add files via upload

--- clube.cpp
+++ clube.cpp
@@ -1,20 +1,33 @@
 #include "clube.h"
 #include "ui_clube.h"
 #include "clb.h"
-#include<QMessageBox>
+#include "QMessageBox"
+#include "QTextDocument"
+#include "QFileDialog"
 #include<QSqlQuery>
-#include<QPrinter>
-#include"QFileDialog"
-#include"QTextDocument"
+#include <QPrinter>
+#include <QPainter>
+#include <QPrintDialog>
+#include <QPieSlice>
+#include <QPieSeries>
+#include <QtCharts/QChartView>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts/QBarCategoryAxis>
+#include"QSortFilterProxyModel"
+QT_CHARTS_USE_NAMESPACE
 Clube::Clube(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Clube)
 {
-    ui->setupUi(this);
+    //ui->setupUi(this);
     ui->tableView->setModel(tmpclb.afficher());
-    ui-> lineEdit->setValidator(new QIntValidator(0, 99999999, this));
+  //  ui-> lineEdit->setValidator(new QIntValidator(0, 99999999, this));
     ui-> lineEdit_2->setInputMask("aaaaaaaaaaaaaaa");
-    ui-> lineEdit_3->setValidator(new QIntValidator(0,99999999, this));
+    //ui-> lineEdit_3->setValidator(new QIntValidator(0,99999999, this));
     clb C;
 
 }
@@ -184,4 +197,74 @@ void Clube::on_lineEdit_6_textChanged(const QString &arg1)
         c.clearTable(ui->tableView);
         int mat=ui->lineEdit_6->text().toInt();
         c.searchRegexp(ui->tableView,mat);
+}
+
+void Clube::on_pushButton_8_clicked()
+{
+    QSqlQueryModel * model= new QSqlQueryModel();
+               model->setQuery("select * from CLUB where TYPE =:chant");
+               float bat1=model->rowCount();
+               model->setQuery("select * from CLUB where TYPE=:musique ");
+               float bat2=model->rowCount();
+               model->setQuery("select * from CLUB where TYPE=:theatre ");
+               float bat3=model->rowCount();
+               model->setQuery("select * from CLUB where TYPE=:arts plastique ");
+               float bat4=model->rowCount();
+               model->setQuery("select * from CLUB where TYPE=:dance ");
+               float bat5=model->rowCount();
+               float total=bat1+bat2+bat3+bat4+bat5;
+               QString a=QString("club chant  "+QString::number((bat1*100)/total,'f',2)+"%" );
+               QString b=QString("club musique  "+QString::number((bat2*100)/total,'f',2)+"%" );
+               QString c=QString("club theatre  "+QString::number((bat3*100)/total,'f',2)+"%" );
+               QString d=QString("club arts plastique  "+QString::number((bat4*100)/total,'f',2)+"%" );
+               QString e=QString("club dance  "+QString::number((bat5*100)/total,'f',2)+"%" );
+               QPieSeries *series = new QPieSeries();
+               series->append(a,bat1);
+               series->append(b,bat2);
+               series->append(c,bat3);
+                series->append(d,bat4);
+                 series->append(e,bat5);
+       if (bat1!=0)
+       {QPieSlice *slice = series->slices().at(0);
+        slice->setLabelVisible();
+        slice->setPen(QPen());}
+       if ( bat2!=0)
+       {
+                // Add label, explode and define brush for 2nd slice
+                QPieSlice *slice1 = series->slices().at(1);
+                //slice1->setExploded();
+                slice1->setLabelVisible();
+       }
+       if(bat3!=0)
+       {
+                // Add labels to rest of slices
+                QPieSlice *slice2 = series->slices().at(2);
+                //slice1->setExploded();
+                slice2->setLabelVisible();
+       }
+       if(bat4!=0)
+       {
+                // Add labels to rest of slices
+                QPieSlice *slice3 = series->slices().at(3);
+                //slice1->setExploded();
+                slice3->setLabelVisible();
+       }
+       if(bat5!=0)
+       {
+                // Add labels to rest of slices
+                QPieSlice *slice4 = series->slices().at(4);
+                //slice1->setExploded();
+                slice4->setLabelVisible();
+       }
+               // Create the chart widget
+               QChart *chart = new QChart();
+               // Add data to chart with title and hide legend
+               chart->addSeries(series);
+               chart->setTitle("Pourcentage Par club :totale de "+ QString::number(total));
+               chart->legend()->hide();
+               // Used to display the chart
+               QChartView *chartView = new QChartView(chart);
+               chartView->setRenderHint(QPainter::Antialiasing);
+               chartView->resize(1000,500);
+               chartView->show();
 }
